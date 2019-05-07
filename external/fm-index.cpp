6fb0822105747c415b1dd63beb3dfb3c505e231c
Arne Ludwig <arne.ludwig@posteo.de>
2019-05-07 12:03:09 +0200

    improved `fm-index`
    - added simple CLI options parser
    - better selection of temporary directory (CLI option with default similar to `mktemp`)
    - fixed throwing of execptions
    - store `recordStarts` in a file for faster subsequent queries

--- external/fm-index.cpp
+++ external/fm-index.cpp
@@ -1,7 +1,9 @@
 #include "fm-index.hpp"
 #include <sdsl/suffix_arrays.hpp>
+#include <sdsl/io.hpp>
 #include <ctime>
-#include <string>
+#include <sys/stat.h>
+#include <cstdlib>
 #include <iostream>
 #include <algorithm>
 #include <iomanip>
@@ -13,49 +15,49 @@ using namespace sdsl;
 using namespace std;
 
 
+char* executable;
+sdsl::cache_config sdsl_config;
 
 int main(int argc, char** argv)
 {
-    if (argc <  2)
-    {
-        cerr << "Usage " << argv[0] << " <in:reference> [<in:queries> ...]" << endl;
-        cerr << "    This program constructs a very compact FM-index" << endl;
-        cerr << "    of <reference> and locates <queries> if given." << endl;
-        cerr << endl;
-        cerr << "Positional arguments:" << endl;
-        cerr << "    <in:refernce>  Original text file with one record per line." << endl;
-        cerr << "    <in:queries>   List of queries (one per line) to locate in <reference>." << endl;
-        cerr << "                   Queries given on standard input will be located before" << endl;
-        cerr << "                   all others." <<endl;
-        cerr << endl;
-        cerr << "Output:" << endl;
-        cerr << "    Creates an FM-index <reference>.fm9 if not present. Then produces" << endl;
-        cerr << "    a list of exact matches for all queries in a TAB-separated format:" << endl;
-        cerr << endl;
-        cerr << "        refName  refId  refLength  queryId  hitBegin  hitEnd" << endl;
-        cerr << endl;
-        cerr << "    IDs and coordinates are zero-based. Coordinates are right-open." << endl;
-        cerr << endl;
-        cerr << "    Note: no output will be produced if no queries are given." << endl;
-
-        return 1;
-    }
+    executable = argv[0];
 
     try
     {
-        string referenceFile = string(argv[1]);
-        char** queriesFiles = &argv[2];
-        int numQueries = max(0, argc - 2);
+        int positionalBegin;
 
-        csa_wt<wt_huff<rrr_vector<127> >, 512, 1024> fmIndex;
+        try
+        {
+            positionalBegin = parseArgs(argc, argv, &(sdsl_config.dir));
+
+            if (argc - positionalBegin <  1)
+                throw FmIndexException("Missing arguments.");
+        }
+        catch (const FmIndexException& e)
+        {
+            cerr << "error: " << e.what() << endl;
+            cerr << endl;
+            printUsage();
+
+            return 1;
+        }
+
+        string referenceFile = string(argv[positionalBegin]);
+        char** queriesFiles = &argv[positionalBegin + 1];
+        int numQueries = max(0, argc - positionalBegin - 1);
+
+        csa_wt<wt_huff<rrr_vector<127> >, 512, 1024> fmIndex(sdsl_config);
         buildIndex(fmIndex, referenceFile);
+        auto recordStarts = getRecordStarts(fmIndex, referenceFile);
 
-        auto recordStarts = getRecordStarts(fmIndex);
+        if (!hasStdin() && numQueries == 0)
+            return 0;
+
         string queryBuffer;
         // Reserve 10 Mb for a single query
         queryBuffer.reserve(10 * (2 << 20));
 
-        if (cin && hasStdin())
+        if (hasStdin())
             locateQueries(fmIndex, recordStarts, cin, "stdin", queryBuffer);
 
         for (int i = 0; i < numQueries; ++i)
@@ -75,20 +77,95 @@ int main(int argc, char** argv)
             locateQueries(fmIndex, recordStarts, queryData, queryFile, queryBuffer);
         }
     }
-    catch (FmIndexException& e)
+    catch (const FmIndexException& e)
     {
         cerr << "error: " << e.what() << endl;
 
         return 2;
     }
-    catch (exception& e)
+    catch (const exception& e)
     {
         cerr << "critical error: " << e.what() << endl;
 
         return 3;
     }
+    catch (...)
+    {
+        cerr << "critical error: Unexpected exception." << endl;
+
+        return 4;
+    }
 
     return 0;
+}
+
+string getDefaultTempDir()
+{
+    char* tempDir;
+
+    #ifdef _GNU_SOURCE
+        tempDir = secure_getenv("TMPDIR");
+    #else
+        tempDir = getenv("TMPDIR");
+    #endif
+
+    if (NULL == tempDir)
+        return "/tmp";
+
+    return string(tempDir);
+}
+
+void printUsage()
+{
+    cerr << "Usage " << executable << " [-P<dir>] <in:reference> [<in:queries> ...]" << endl;
+    cerr << "    This program constructs a very compact FM-index" << endl;
+    cerr << "    of <reference> and locates <queries> if given." << endl;
+    cerr << endl;
+    cerr << "Positional arguments:" << endl;
+    cerr << "    <in:refernce>  Original text file with one record per line." << endl;
+    cerr << "    <in:queries>   List of queries (one per line) to locate in <reference>." << endl;
+    cerr << "                   Queries given on standard input will be located before" << endl;
+    cerr << "                   all others." <<endl;
+    cerr << endl;
+    cerr << "Optional arguments:" << endl;
+    cerr << "    -P<dir>        Use <dir> as temporary directory (default: " << getDefaultTempDir() << ")." << endl;
+    cerr << "    <in:queries>   List of queries (one per line) to locate in <reference>." << endl;
+    cerr << "                   Queries given on standard input will be located before" << endl;
+    cerr << "                   all others." <<endl;
+    cerr << endl;
+    cerr << "Output:" << endl;
+    cerr << "    Creates an FM-index <reference>.fm9 if not present. Then produces" << endl;
+    cerr << "    a list of exact matches for all queries in a TAB-separated format:" << endl;
+    cerr << endl;
+    cerr << "        refName  refId  refLength  queryId  hitBegin  hitEnd" << endl;
+    cerr << endl;
+    cerr << "    IDs and coordinates are zero-based. Coordinates are right-open." << endl;
+    cerr << endl;
+    cerr << "    Note: no output will be produced if no queries are given." << endl;
+}
+
+int parseArgs(int argc, char** argv, string *tempDir)
+{
+    *tempDir = getDefaultTempDir();
+
+    int i;
+    for (i = 1; i < argc && argv[i][0] == '-'; ++i)
+        switch (argv[i][1])
+        {
+            case 'P':
+                *tempDir = string(argv[i] + 2);
+
+                if (tempDir->size() == 0)
+                    throw FmIndexException("Missing value for -P.");
+                break;
+            default:
+                throw FmIndexException(string("Invalid option -") + argv[i][1] + ".");
+        }
+
+    if (!dirExists(*tempDir))
+        throw FmIndexException(string("Cannot open temporary directory: ") + *tempDir);
+
+    return i;
 }
 
 void buildIndex(csa_wt<wt_huff<rrr_vector<127> >, 512, 1024> &fmIndex, string referenceFile)
@@ -96,11 +173,12 @@ void buildIndex(csa_wt<wt_huff<rrr_vector<127> >, 512, 1024> &fmIndex, string re
     string indexSuffix = ".fm9";
     string indexFile = referenceFile + indexSuffix;
 
-    if (!load_from_file(fmIndex, indexFile)) {
+    if (!load_from_file(fmIndex, indexFile))
+    {
         ifstream referenceData(referenceFile.c_str());
 
         if (!referenceData)
-            throw new FmIndexException("File `" + referenceFile + "` does not exist.");
+            throw FmIndexException("File `" + referenceFile + "` does not exist.");
 
         cerr << "{\"level\":\"info\","
              << "\"info\":\"Index does not exist. Building it now.\","
@@ -121,15 +199,40 @@ void buildIndex(csa_wt<wt_huff<rrr_vector<127> >, 512, 1024> &fmIndex, string re
     }
 }
 
-vector<size_t> getRecordStarts(csa_wt<wt_huff<rrr_vector<127> >, 512, 1024> &fmIndex)
+vector<size_t> getRecordStarts(csa_wt<wt_huff<rrr_vector<127> >, 512, 1024> &fmIndex, string referenceFile)
 {
-    auto lineEndLocations = locate(fmIndex, "\n");
-    sort(lineEndLocations.begin(), lineEndLocations.end());
-    vector<size_t> recordStarts(lineEndLocations.size() + 1);
+    string recordIndexSuffix = ".idx";
+    string recordIndexFile = referenceFile + recordIndexSuffix;
+    vector<size_t> recordStarts;
 
-    recordStarts[0] = 0;
-    for (int i = 1; i < recordStarts.size(); ++i)
-        recordStarts[i] = lineEndLocations[i - 1] + 1;
+    if (!load_from_plain_array<size_t>(recordStarts, recordIndexFile))
+    {
+        cerr << "{\"level\":\"info\","
+             << "\"info\":\"Record index does not exist. Building it now.\","
+             << "\"file\":\"" << recordIndexFile << "\""
+             << "}" << endl;
+
+        time_t start = time(NULL);
+
+        auto lineEndLocations = locate(fmIndex, "\n");
+        sort(lineEndLocations.begin(), lineEndLocations.end());
+        recordStarts.resize(lineEndLocations.size() + 1);
+
+        recordStarts[0] = 0;
+        for (int i = 1; i < recordStarts.size(); ++i)
+            recordStarts[i] = lineEndLocations[i - 1] + 1;
+
+        if (!store_to_plain_array<size_t>(recordStarts, recordIndexFile))
+            throw FmIndexException("Could not store record index: " + recordIndexFile);
+
+        cerr << "{\"level\":\"info\","
+             << "\"info\":\"Built record index.\","
+             << "\"file\":\"" << recordIndexFile << "\","
+             << "\"numRecords\":" << recordStarts.size() - 1 << ","
+             << "\"elpasedSecs\":" << difftime(time(NULL), start) << ","
+             << "\"sizeMiB\":" << size_in_mega_bytes(recordStarts)
+             << "}" << endl;
+    }
 
     return recordStarts;
 }
@@ -217,5 +320,12 @@ size_t findSourceId(vector<size_t> recordStarts, size_t hitBegin)
         if (recordStarts[i] <= hitBegin && hitBegin < recordStarts[i + 1])
             return i;
 
-    throw new FmIndexException("Invalid hit: cannot associate a record.");
+    throw FmIndexException("Invalid hit: cannot associate a record.");
+}
+
+bool dirExists(const string& dir)
+{
+    struct stat dirStat;
+
+    return 0 == stat(dir.c_str(), &dirStat) && ((dirStat.st_mode & S_IFMT) == S_IFDIR);
 }
--- external/fm-index.hpp
+++ external/fm-index.hpp
@@ -12,8 +12,10 @@ using namespace std;
 #define INCLUDED_FM_INDEX_HPP
 
 int main(int argc, char** argv);
+void printUsage();
+int parseArgs(int argc, char** argv, string *tempDir);
 void buildIndex(csa_wt<wt_huff<rrr_vector<127> >, 512, 1024> &fmIndex, string referenceFile);
-vector<size_t> getRecordStarts(csa_wt<wt_huff<rrr_vector<127> >, 512, 1024> &fmIndex);
+vector<size_t> getRecordStarts(csa_wt<wt_huff<rrr_vector<127> >, 512, 1024> &fmIndex, string referenceFile);
 int hasStdin();
 void locateQueries(
     csa_wt<wt_huff<rrr_vector<127> >, 512, 1024> &fmIndex,
@@ -31,6 +33,30 @@ size_t locateQuery(
 );
 size_t findSourceId(vector<size_t> recordStarts, size_t hitBegin);
 
+template<class int_type, class t_int_vec>
+bool load_from_plain_array(t_int_vec& v, const std::string& file)
+{
+    ifstream in(file, std::ios::binary | std::ios::in);
+
+    if (!in)
+        return false;
+
+    // get length of file
+    in.seekg (0, in.end);
+    int fileSize = in.tellg();
+    in.seekg (0, in.beg);
+
+    if (fileSize % sizeof(int_type) != 0)
+        return false;
+
+    v.resize(fileSize / sizeof(int_type));
+
+    in.read((char*) v.data(), fileSize);
+
+    return (bool) in;
+}
+
+bool dirExists(const string& dir);
 
 class FmIndexException : public exception {
     const string msg;
