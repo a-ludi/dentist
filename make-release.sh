9bfe015a429149ecc65356f7a7c7a8e102032499
Arne Ludwig <arne.ludwig@posteo.de>
2020-02-12 16:07:18 +0100

    Added `make-release.sh` to create binary releases

---
+++ make-release.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+# Unofficial Bash Strict Mode (http://redsymbol.net/articles/unofficial-bash-strict-mode/)
+set -euo pipefail
+IFS=$'\n\t'
+PROG="$(basename "$0")"
+
+
+function error()
+{
+    if (( $# > 0 ));
+    then
+        echo "$PROG: error:" "$@"
+    else
+        echo
+    fi
+} >&2
+
+
+function bail_out()
+{
+    error "$@"
+    exit 1
+}
+
+
+function bail_out_usage()
+{
+    error "$@"
+    error
+    print_usage
+    exit 1
+}
+
+
+function log()
+{
+    echo "--" "$@"
+} >&2
+
+
+function print_usage()
+{
+    echo "USAGE:  $PROG [-h]"
+} >&2
+
+
+function print_help()
+{
+    print_usage
+    echo
+    echo 'Build DENTIST in release mode and create a tarball.'
+    echo
+    echo 'Optional arguments:'
+    echo ' --help, -h      Prints this help.'
+    echo ' --usage         Print a short command summary.'
+    echo ' --version       Print software version.'
+} >&2
+
+
+function print_version()
+{
+    echo "$PROG v0"
+    echo
+    echo "Copyright © 2019, Arne Ludwig <arne.ludwig@posteo.de>"
+} >&2
+
+
+function parse_args()
+{
+    ARGS=()
+    for ARG in "$@";
+    do
+        if [[ "${ARG:0:1}" == - ]];
+        then
+            case "$ARG" in
+                -h|--help)
+                    print_help
+
+                    exit
+                    ;;
+                --usage)
+                    print_usage
+
+                    exit
+                    ;;
+                --version)
+                    print_version
+
+                    exit
+                    ;;
+                *)
+                    bail_out_usage "unkown option $ARG"
+                    ;;
+            esac
+        else
+            ARGS+=( "$ARG" )
+        fi
+    done
+
+    (( ${#ARGS[*]} == 0 )) || bail_out_usage "too many arguments"
+}
+
+
+function main()
+{
+    DENTIST=dentist
+
+    parse_args "$@"
+
+    dub build --build=release
+    strip -s "$DENTIST"
+
+    DENTIST_VERSION="$("$DENTIST" --version |& head -n1)"
+    DENTIST_VERSION="${DENTIST_VERSION#dentist }"
+    ARCH="$(uname -m)"
+    TARBALL="dentist.$DENTIST_VERSION.$ARCH.tar.gz"
+
+    tar -czf "$TARBALL" dentist
+
+    log "created $TARBALL"
+}
+
+
+main "$@"
