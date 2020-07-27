a-ludi/dentist

commit ccb126987d3483d64aff4ec9b7f4cd73f9298747

Removed CLI option `--trace-point-spacing`; now always inferred

source/dentist/commandline.d
@@ -1444,7 +1444,7 @@ struct OptionsFor(DentistCommand _command)
         @Validate!(
             (value, options) => enforce!CLIException(
                 value > options.tracePointDistance,
-                "minimum anchor length should be greater than --trace-point-spacing"
+                "minimum anchor length should be greater than trace point spacing of *.las file"
             ),
             is(typeof(OptionsFor!command().tracePointDistance)),
         )
@@ -1709,28 +1709,15 @@ struct OptionsFor(DentistCommand _command)
         DentistCommand.output,
     ))
     {
-        @Option("trace-point-spacing", "s")
-        @Help("trace point spacing used for the ref vs. reads alignment")
         trace_point_t tracePointDistance;
 
-        @PostValidate(Priority.medium)
-        void hookEnsurePresenceOfTracePointDistance()
+        @PreValidate(Priority.medium)
+        void hookGetTracePointDistance()
         {
-            if (tracePointDistance > 0)
-                return;
-
             static if (is(typeof(readsDb)) && is(typeof(readsAlignmentFile)))
-                tracePointDistance = getTracePointDistance(
-                    refDb,
-                    readsDb,
-                    readsAlignmentFile,
-                );
+                tracePointDistance = getTracePointDistance(readsAlignmentFile);
             else static if (is(typeof(dbAlignmentFile)))
-                tracePointDistance = getTracePointDistance(
-                    refDb,
-                    readsDb,
-                    dbAlignmentFile,
-                );
+                tracePointDistance = getTracePointDistance(dbAlignmentFile);
         }
     }
 

source/dentist/commands/output.d
@@ -296,7 +296,7 @@ class AssemblyWriter
         assemblyGraph = assemblyGraph
             .enforceJoinPolicy!InsertionInfo(options.joinPolicy, forbiddenInsertions)
             .normalizeUnkownJoins!InsertionInfo()
-            .fixCropping(options.tracePointDistance);
+            .fixCropping();
         incidentEdgesCache = assemblyGraph.allIncidentEdges();
 
         foreach (forbiddenInsertion; forbiddenInsertions)
@@ -781,7 +781,6 @@ class AssemblyWriter
 /// cropping where cropped regions overlap.
 OutputScaffold fixCropping(
     OutputScaffold scaffold,
-    trace_point_t tracePointDistance,
 )
 {
     mixin(traceExecution);

source/dentist/commands/processPileUps/cropper.d
@@ -64,7 +64,6 @@ struct CropOptions
     string refDb;
     string readsDb;
     coord_t minAnchorLength;
-    trace_point_t tracePointDistance;
     string workdir;
 }
 
@@ -147,7 +146,9 @@ private struct PileUpCropper
                         "info", "could not find a common trace point",
                         "croppingRefPositions", croppingRefPositions.toJson,
                         "croppingSeeds", croppingSeeds.toJson,
-                        "tracePointDistance", options.tracePointDistance,
+                        "tracePointDistance", pileUp.length > 0
+                            ? pileUp[0][0].tracePointDistance
+                            : 0,
                         "pileUp", [
                             "type": pileUp.getType.to!string.toJson,
                             "readAlignments": shouldLog(LogLevel.debug_)
@@ -229,7 +230,7 @@ private struct PileUpCropper
     {
         auto alignmentsByContig = pileUp.splitAlignmentsByContigA();
         auto commonTracePoints = alignmentsByContig
-            .map!(acs => getCommonTracePoint(acs, repeatMask, options.tracePointDistance));
+            .map!(acs => getCommonTracePoint(acs, repeatMask));
         auto contigAIds = alignmentsByContig.map!"a[0].contigA.id";
         auto seeds = alignmentsByContig.map!"a[0].seed".array;
 
@@ -383,7 +384,6 @@ private SeededAlignment[][] splitAlignmentsByContigA(PileUp pileUp)
 private long getCommonTracePoint(
     in SeededAlignment[] alignments,
     in ReferenceRegion mask,
-    in size_t tracePointDistance,
 )
 {
     static long _getCommonTracePoint(R)(R tracePointCandidates, ReferenceRegion tracePointRegion) pure
@@ -396,6 +396,7 @@ private long getCommonTracePoint(
 
     auto contigA = alignments[0].contigA;
     auto locationSeed = alignments[0].seed;
+    auto tracePointDistance = alignments[0].tracePointDistance;
     auto commonAlignmentRegion = alignments
         .map!(to!(ReferenceRegion, "contigA"))
         .fold!"a & b";

source/dentist/commands/processPileUps/package.d
@@ -398,7 +398,6 @@ protected class PileUpProcessor
             options.refDb,
             options.readsDb,
             options.minAnchorLength,
-            options.tracePointDistance,
             options.workdir,
         ));
 

commit ac71be0d900a64fab0d47da4a5b8693371eb8279

Removed `tracePointDistance` from `output`
- fixes error introduced by commit ccb126987d3483d64aff4ec9b7f4cd73f9298747

source/dentist/commandline.d
@@ -1706,7 +1706,6 @@ struct OptionsFor(DentistCommand _command)
         DentistCommand.maskRepetitiveRegions,
         DentistCommand.collectPileUps,
         DentistCommand.processPileUps,
-        DentistCommand.output,
     ))
     {
         @(Validate!(validatePositive!("trace-point-distance", trace_point_t)))