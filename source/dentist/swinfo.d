6826e706d9a865692118cd3c02e1147c7484dd14
Arne Ludwig <arne.ludwig@posteo.de>
2021-12-09 15:01:32 +0100

    Bump version

--- source/dentist/swinfo.d
+++ source/dentist/swinfo.d
@@ -12,8 +12,8 @@ import dentist.common : testingOnly;
 import std.string : wrap;
 
 enum executableName = "dentist";
-enum gitVersion = "v2.0.0";
-enum gitCommit = "5785bb3913734394cc1cd1233713ac7361def180";
+enum gitVersion = "v3.0.0";
+enum gitCommit = "316657c595c4a56c2e0a40adaeabe98328a205ba";
 enum version_ = gitVersion ~ testingOnly!"+testing";
 enum description = "Close assembly gaps using long-reads with focus on correctness.".wrap;
 enum copyright = "Copyright © 2018, Arne Ludwig <arne.ludwig@posteo.de>".wrap;
